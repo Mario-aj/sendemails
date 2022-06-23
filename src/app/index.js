85e99d9d2db2e06b7df0364d360c1772f9d4893c
mario-aj <mariojorge1997@mail.ru>
2022-06-23 14:27:06 +0100

    read emails from txt file

--- src/app/index.js
+++ src/app/index.js
@@ -1,29 +1,31 @@
 require("dotenv").config();
 
-const { target } = require("./target");
+const { targets } = require("./target");
 const { transporter } = require("./nodemailer");
 const { readHTMLTemplate } = require("./readHtmlTemplate");
 
 async function send() {
   readHTMLTemplate(__dirname + "/template.html", async (template) => {
-    const html = template({ username: target.username });
     const sender = `"Mário Alfredo Jorge" ${process.env.USER_EMAIL}`;
-
-    await transporter
-      .sendMail({
-        from: sender,
-        to: `${target.email}`,
-        subject: "Frontend developer",
-        text: "Hey!",
-        html,
-      })
-      .then(() => console.log("Email sent to: " + target.email))
-      .catch((error) =>
-        console.error(
-          "We have a problem to send email to " + target.email + ": ",
-          error
-        )
-      );
+    const subject = "Mensagem automática";
+    await targets?.forEach(async (target) => {
+      const html = template({ username: target?.username });
+      await transporter
+        .sendMail({
+          from: sender,
+          to: target?.email,
+          subject: target?.subject || subject,
+          text: "Hey!",
+          html,
+        })
+        .then(() => console.log("Email sent to: " + target?.email))
+        .catch((error) =>
+          console.error(
+            "We have a problem to send email to " + target?.email + ": ",
+            error
+          )
+        );
+    });
   });
 }
 
--- src/app/target.js
+++ src/app/target.js
@@ -1,6 +1,21 @@
-const target = {
-  email: "www.maurok@gmail.com",
-  username: "FooBar",
-};
+const fs = require("fs");
 
-module.exports = { target };
+function getTarget() {
+  fs.readFile(__dirname + "/targets.txt", "utf8", (err, data) => {
+    if (err) throw err;
+    const targets = data.split("\n").map((line) => {
+      const [email, subject] = line.split(" ");
+      const username = email
+        .split("@")[0]
+        .replace(/[^a-zA-Z0-9]/g, "")
+        .replace(/[1-9]/g, "");
+      return { email, subject, username };
+    });
+
+    return targets || [];
+  });
+}
+
+const targets = getTarget();
+
+module.exports = { targets };
