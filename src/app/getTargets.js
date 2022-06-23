204fb9bab830cc07cff2699132ca3b118ef37dd6
mario-aj <mariojorge1997@mail.ru>
2022-06-23 17:44:30 +0100

    prevent error when there is no  email or subject

--- src/app/getTargets.js
+++ src/app/getTargets.js
@@ -3,13 +3,13 @@ const fs = require("fs");
 async function getTargets() {
   const data = fs.readFileSync(__dirname + "/targets.txt", "utf8");
 
-  const targets = data.split("\n").map((line) => {
+  const targets = data.split("\n")?.map((line) => {
     let [email, subject] = line.split(" ");
     const username = email
       .split("@")[0]
       .replace(/[^a-zA-Z0-9]/g, "")
       .replace(/[1-9]/g, "");
-    subject = subject.replace(/_/g, " ");
+    subject = subject?.replace(/_/g, " ");
     return { email, subject, username };
   });
 
